cb8ae2071b36ffcc8570dba2a28601dba121cb8b
Sarasraman <sarath0826@yahoo.com>
2024-02-19 20:41:30 +0530

    Learn About Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+import Button from "./ui/Button";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Heading>Hello World</Heading>
+      <Button size="small" variation="danger">
+        Check
+      </Button>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  font-size: 20px;
+  font-weight: 600;
+  background-color: yellow;
+  color: black;
+`;
+
+export default Heading;
