88cb67c6cc291d06908262a5edf9080f7b3db506
Sarasraman <sarath0826@yahoo.com>
2024-02-20 14:26:07 +0530

    Handle forms using react-hook-forms

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizantal">
@@ -11,6 +15,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((showForm) => !showForm)}>
+          Add New Cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
