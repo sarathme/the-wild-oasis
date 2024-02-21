e6981a9e1b256da22f28ba659b4bd55b596cbef2
Sarasraman <sarath0826@yahoo.com>
2024-02-21 16:50:34 +0530

    Implent closing model when clicked outside the modal

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+export function useOutsideClick(handler, listenCapturing = true) {
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        handler();
+      }
+    }
+    document.addEventListener("click", handleClickOutside, listenCapturing);
+
+    return () =>
+      document.removeEventListener(
+        "click",
+        handleClickOutside,
+        listenCapturing
+      );
+  });
+
+  return ref;
+}
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
 import { createPortal } from "react-dom";
-import { cloneElement, createContext, useContext, useState } from "react";
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -73,11 +81,12 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
 
   if (name !== openName) return null;
   return createPortal(
-    <Overlay onClick={close}>
-      <StyledModal>
+    <Overlay>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
