82bf541cdeb6134a8388f4a804fc4bbd03312925
Sarasraman <sarath0826@yahoo.com>
2024-02-29 22:47:04 +0530

    Implement filtering and sorting on clientside and fetch the data

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //FILTER
+  const filteredValue = searchParams.get("status");
+
+  const filter =
+    !filteredValue || filteredValue === "all"
+      ? null
+      : { field: "status", value: filteredValue };
+
+  //SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-asc";
+  console.log(sortByRaw);
+  const [field, direction] = sortByRaw.split("-");
+
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
