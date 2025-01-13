7d16732a4692e003295c0de51077347084ce41f8
Sarasraman <sarathsatheesh603@gmail.com>
2025-01-13 12:49:04 +0530

    Add supabase credentials in env file

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("sarath@example.com");
-  const [password, setPassword] = useState("784512963");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isPending } = useLogin();
 
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://cvtmgfsnvhvbawewtwjt.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2dG1nZnNudmh2YmF3ZXd0d2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgzNTkxNzgsImV4cCI6MjAyMzkzNTE3OH0.K3TUysGqrg8cHHtiM4-CPHQ2lG1GaqvYwL5bP_HhsRU";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
